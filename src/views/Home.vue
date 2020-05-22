d4ea0013654afae695b049ccf3207fe6133f8ce8
Bulat/A <bulatahtyamov1995@mail.ru>
2020-05-22 13:06:58 +0300

    added vue crash course

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2>Home page</h2>
+
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+      architecto incidunt dolor, laudantium ducimus, iusto temporibus iste esse
+      modi officiis inventore alias veritatis optio maxime. Inventore omnis odit
+      possimus libero.
+    </p>
+
+    <router-link to="/todos">Todos</router-link>
+  </div>
+</template>
